12911987bfe6190ab04c310c1f799c6b0b99fbd9
V-gy <vgy@hotmail.fr>
2019-10-10 17:24:33 +0200

    changement scroll pour la navbar

--- js/interaction.js
+++ js/interaction.js
@@ -1,61 +1,57 @@
 // code pour le bouton Formulaire 
 function askForm() {
-    let email = document.getElementById("email").value;
-    let name = document.getElementById("name").value;
-    let select_form = document.getElementById("select_form").value;
+  let email = document.getElementById("email").value;
+  let name = document.getElementById("name").value;
+  let select_form = document.getElementById("select_form").value;
   console.log(email, name, select_form)
-    //    const message = "Hello " +email+ + name + +select_form+ ", your registre es en cours de validation ! ";           
+  //    const message = "Hello " +email+ + name + +select_form+ ", your registre es en cours de validation ! ";           
   // alert(message);
 }
 //Bouton formulaire ouverture modal  //
 const firstBtn = document.getElementById("open-modal");
-firstBtn.addEventListener("click", function(event){
+firstBtn.addEventListener("click", function (event) {
   document.getElementById("modal").style.display = 'block';
 })
 
 // // //  Boutton formulaire fermeture modal //
 const twoBtn = document.getElementById("close-modal");
-twoBtn.addEventListener("click", function(event){
+twoBtn.addEventListener("click", function (event) {
   document.getElementById("modal").style.display = 'none';
- })
+})
 
- //Bouton Menu burger
- window.onload=function(){
+//Bouton Menu burger
+window.onload = function () {
   let bouton = document.getElementById('burger');
   let nav = document.getElementById('menu');
-  bouton.addEventListener("click",function(event){
-      if(nav.style.display=="block"){
-          nav.style.display="none";
-      }else{
-          nav.style.display="block";
-        
-      }
-    });
+  bouton.addEventListener("click", function (event) {
+    if (nav.style.display == "block") {
+      nav.style.display = "none";
+    } else {
+      nav.style.display = "block";
+
+    }
+  });
 };
 
 //Bouton fermeture popup cookies  //
-const popupBtn = document.getElementById("cookies_button1");
-popupBtn.addEventListener("click", function(event){
-document.getElementById("popup_cookies").style.display = 'none';
-})
+// const popupBtn = document.getElementById("cookies_button1");
+// popupBtn.addEventListener("click", function (event) {
+//   document.getElementById("popup_cookies").style.display = 'none';
+// })
 
-/*window.scroll = fonction(){
-  let translucid = document.getElementById('translucid');
-
-window.addEventListener("scroll", function(scrollTop){
-   
-  if (window.scrollTop < 10){
-    window.alert("texte")
-    translucid.style.display="none";
+//changement avec le scroll de la navbar!!!!
+function checkNavbar() {
+  let element = document.querySelector("#barre-de-nav");
+  if (window.scrollY < 100) {
+    element.style.display = "none";
+  }else{
+    element.style.display = "flex";
   }
-})
 }
-window.scrollTop(window.addEventListener("scroll", => window.scrollTop{
-  if(window.scrollTop<10)
 
-}))
-$(window).scroll(function () {
-  var y = $(window).scrollTop();
-  if (y>0){
-  $('translucid').hide;}
-});*/
+window.addEventListener(
+  "scroll",
+  checkNavbar
+);
+
+checkNavbar();
